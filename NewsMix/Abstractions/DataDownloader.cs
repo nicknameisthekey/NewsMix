52f3efbff1208b22031c9861f5ba98d4bcf7df42
Stanislaw Babkin <stalissss@outlook.com>
2023-01-16 10:19:11 +0300

    Remove redundant code

--- NewsMix/Abstractions/DataDownloader.cs
+++
@@ -1,9 +0,0 @@
-using NewsMix.Models;
-
-namespace NewsMix.Abstractions;
-
-public interface DataDownloader
-{
-    Task<Page> GetPage(string uri, DownloadMethod downloadMethod);
-    Task<byte[]?> DownloadFile(string url);
-}
--- NewsMix/UI/Telegram/Models/KeyboardLayer.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace NewsMix.UI.Telegram.Models;
-public class KeyboardLayer
-{
-    public InlineKeyboardButton[] Layer { get; set; } = null!;
-}
