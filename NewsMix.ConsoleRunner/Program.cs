bebe71792c6809a3acc7b67cb79edea73d6aa952
Владислав Барсуков <vladislavbarsukov@Vladislavs-MBP.home>
2024-02-04 15:09:17 +0500

    add serilog

--- NewsMix.ConsoleRunner/Program.cs
+++ NewsMix.ConsoleRunner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace NewsMix.ConsoleRunner;
 
@@ -12,11 +13,12 @@ public class Program
             .ConfigureServices((ctx, services) =>
             {
                 services.AddNewsMix(ctx.Configuration, true);
-                services.AddLogging(b => b
-                    .AddDebug()
-                    .AddConsole());
 
                 services.Configure<HostOptions>(options => { options.ShutdownTimeout = TimeSpan.FromSeconds(2); });
+            })
+            .UseSerilog((ctx, conf) =>
+            {
+                conf.ReadFrom.Configuration(ctx.Configuration);
             })
             .Build();
         var logger = host.Services.GetRequiredService<ILogger<Program>>();
